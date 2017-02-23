3bfd97fcb3cac278e47b1386fbbe2445b6367af9
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2017-02-23 16:39:46 +0100

    a

---
+++ elixir/loadgenerator/lib/loadgenerator.ex
@@ -0,0 +1,18 @@
+defmodule Flink.Load do
+  @moduledoc """
+  Documentation for Flink.Load.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flink.Load.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/loadgenerator/test/loadgenerator_test.exs
@@ -0,0 +1,8 @@
+defmodule Flink.LoadTest do
+  use ExUnit.Case
+  doctest Flink.Load
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
