fb1f5b391408e0dce0b93864fa8589b6e3bf11d3
Christian Geuer-Pollmann <christian.geuer-pollmann@web.de>
2017-02-23 15:26:58 +0100

    a

---
+++ docs/open-ssh.cmd
@@ -0,0 +1,7 @@
+
+set USERNAME=chgeuer
+set SSHPRIV=%USERPROFILE%\Java\keys\dcos.putty.ppk
+set IP=13.73.154.72
+
+pageant.exe %SSHPRIV%
+putty.exe -ssh -2 -l %USERNAME% -L 8081:localhost:8081 -P 22 -agent -A -i %SSHPRIV% %IP%
